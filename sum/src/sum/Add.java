71340e04bef0bb9a72f5ddd28b63a39df50bfd72
sonu choudhary <sonuchoudhary987654@gmail.com>
2021-12-30 11:56:30 -0800

    intital commit

---
+++ sum/src/sum/Add.java
@@ -0,0 +1,13 @@
+package sum;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+
+	}
+
+}
